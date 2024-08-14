1be373497cdfbb5d8033adbeafd6b03e52b96681
Awais <157953102+MAwaisAleem@users.noreply.github.com>
2024-08-14 20:22:51 +0500

    Updated code to fix image imports and display issues

---
+++ src/Data.jsx
@@ -0,0 +1,53 @@
+import iphone11 from "./assets/images/products/iphone11.png";
+import iphone11ProMax from "./assets/images/products/iphone11promax.png";
+import iphone12Mini from "./assets/images/products/iphone12mini.png";
+import iphone12 from "./assets/images/products/iphone12.png";
+import iphone12Pro from "./assets/images/products/iphone12pro.png";
+import iphone12ProMax from "./assets/images/products/iphone12promax.png";
+
+const DATA = [
+  {
+    id: 0,
+    title: "IPhone 11",
+    price: 750,
+    desc: "Contrary to popular belief, Lorem Ipsum is not simply random text...",
+    img: iphone11,
+  },
+  {
+    id: 1,
+    title: "IPhone 11 Pro Max",
+    price: 800,
+    desc: "Contrary to popular belief, Lorem Ipsum is not simply random text...",
+    img: iphone11ProMax,
+  },
+  {
+    id: 2,
+    title: "IPhone 12 Mini",
+    price: 950,
+    desc: "Contrary to popular belief, Lorem Ipsum is not simply random text...",
+    img: iphone12Mini,
+  },
+  {
+    id: 3,
+    title: "IPhone 12",
+    price: 1050,
+    desc: "Contrary to popular belief, Lorem Ipsum is not simply random text...",
+    img: iphone12,
+  },
+  {
+    id: 4,
+    title: "IPhone 12 Pro",
+    price: 1150,
+    desc: "Contrary to popular belief, Lorem Ipsum is not simply random text...",
+    img: iphone12Pro,
+  },
+  {
+    id: 5,
+    title: "IPhone 12 Pro Max",
+    price: 1250,
+    desc: "Contrary to popular belief, Lorem Ipsum is not simply random text...",
+    img: iphone12ProMax,
+  },
+];
+
+export default DATA;
